72e9203a07040587fbec747718da13eb2d5cfcd9
James Park <jamestkd918@gmail.com>
2018-08-28 15:18:34 -0700

    Adding viewset component

---
+++ src/components/ViewSet.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Viewset extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+Viewset.propTypes = {
+
+};
+
+export default Viewset
