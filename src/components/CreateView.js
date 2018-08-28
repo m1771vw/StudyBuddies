47dbcae6b500e0edde98cb2b357f1029bf6b23c6
William Yang <williamyang93@gmail.com>
2018-08-28 15:12:33 -0700

    Added dummy data and Create View component

---
+++ src/components/CreateView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class CreateView extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+CreateView.propTypes = {
+
+};
+
+export default CreateView;
---
+++ src/data/flashcards.js
@@ -0,0 +1,28 @@
+const flashCards = [
+    {
+        "setname": "Biology",
+        "description" : "Da science",
+        "creator": "James",
+        "cards" : [
+            {
+                "term" : "penis",
+                "definition" : "da dick"
+            },
+            {
+                "term" : "vagina",
+                "definition" : "da pussy"
+            }
+        ]
+    },
+    {
+        "setname": "History",
+        "description" : "Da history",
+        "creator": "James",
+        "cards" : [
+            {
+                "term" : "china #2",
+                "definition" : "taiwan #1"
+            }
+        ]
+    }
+]
