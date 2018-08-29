e7cd3403dd9ef1097964a42b3c287ece20c91d14
James Park <jamestkd918@gmail.com>
2018-08-29 11:54:38 -0700

    CreatView outline/frame finished. Adding css styling.

--- src/components/CreateView.js
+++ src/components/CreateView.js
@@ -3,39 +3,43 @@ import React, { Component } from 'react';
 
 class CreateView extends Component {
 
-
+createRow = () =>{
     
+}
+
     render() {
         return (
             <div>
                 <div className="level">
                     <div className="level-item has-text-centered">
-                    <input className="input is-primary" type="text" placeholder="Term" />
-                        
+                        <input className="input is-primary set-name-field" type="text" placeholder="Enter set name here..." />
+
                     </div>
                 </div>
 
-                <div className="level">
-                    <div className="level-item has-text-centered">
-                    <textarea class="textarea" placeholder="Enter description here..."></textarea>
-                    </div>
-                </div>   
+                <div className="level text-area-level">
+                        <textarea class="textarea" placeholder="Enter description here..." rows="7"></textarea>
+                </div>
 
                 <div className="field level">
-                    <div className="term div"> 
+                    <div className="term-div">
                         <input className="input is-primary" type="text" placeholder="Term" />
                         <input className="input is-primary" type="text" placeholder="Term" />
                         <input className="input is-primary" type="text" placeholder="Term" />
                         <input className="input is-primary" type="text" placeholder="Term" />
                     </div>
-                    <div className ="definition-div">
+                    <div className="definition-div">
                         <input className="input is-primary" type="text" placeholder="Definition" />
                         <input className="input is-primary" type="text" placeholder="Definition" />
                         <input className="input is-primary" type="text" placeholder="Definition" />
-                        <input className="input is-primary" type="text" placeholder="Definition" /> 
+                        <input className="input is-primary" type="text" placeholder="Definition" />
                     </div>
                 </div>
-               
+                <div className="level" id="createbutton1">
+                <button className="button is-large level-item">+</button>
+                <button className="button is-large level-item">Submit Set</button>
+                </div>
+
 
             </div>
         );
