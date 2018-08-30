68f045f3b111261430435c6a8f33e1e7cf657f9d
William Yang <williamyang93@gmail.com>
2018-08-30 16:46:09 -0700

    Changed ViewSet to class and added toggle for terms vs definition

--- src/components/ViewSet.js
+++ src/components/ViewSet.js
@@ -1,36 +1,51 @@
-import React from 'react';
-import {  QUIZ, EDIT } from '../constants'
+import React, { Component } from 'react';
+import {  QUIZ, EDIT } from '../constants';
 import { func, object } from 'prop-types';
 
 
-const Viewset = props => {
-    return (
+class Viewset extends Component {
+    state = {
+        showTerms: true
+    }
+
+    showOtherSide = () => {
+        this.setState({
+            showTerms: !this.state.showTerms
+        })
+    }
+    render() {
+        return (
         <div>
             <div className="level">
                 <div className="cardset-info-flex-container">
                     <div className='cardset-setname-flex-container'>
                         <div className="box setname-box">
                             <h1>SET NAME</h1>
-                            <h2>{props.selectedCardSet.setname}</h2>
+                            <h2>{this.props.selectedCardSet.setname}</h2>
                         </div>
                         <div className='cardset-options-flex-container'>
-                            <button className="button is-danger" onClick={() => {props.changePageName(EDIT)}}>Edit</button>
-                            <button className="button is-success" onClick={() => {props.setupQuiz(QUIZ, props.selectedCardSet)}}>Quiz!</button>
+                            <button className="button is-danger" onClick={() => {this.props.changePageName(EDIT)}}>Edit</button>
+                            <button className="button is-success" onClick={() => {this.props.setupQuiz(QUIZ, this.props.selectedCardSet)}}>Quiz!</button>
                         </div>
                     </div>
                     <div className="box description-box">
                         <h1>Description</h1>
-                        <p>{props.selectedCardSet.description}</p>
+                        <p>{this.props.selectedCardSet.description}</p>
                     </div>
                 </div>
             </div>
             <div className='level'>
                 <div className='flashcard-flex-container'>
-                    {props.selectedCardSet.cards.map( (card, index) => {
-                        return(
+                    {this.props.selectedCardSet.cards.map( (card, index) => {
+                        return( 
                         <div key={card+index} className='level-item level-left'>
-                            <div className="box flashcard-box">
-                                <h1>{card.term}</h1>
+                            <div onClick={this.showOtherSide} className="box flashcard-box">
+                                {
+                                    this.state.showTerms
+                                    ? <h1>{card.term}</h1>
+                                    : <h1>{card.definition}</h1>
+                                }
+                                
                             </div>
                         </div>
                         
@@ -39,7 +54,7 @@ const Viewset = props => {
             </div>
         </div>
     );
-
+    }
 }
 
 Viewset.propTypes = {
