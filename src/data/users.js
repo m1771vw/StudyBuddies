ce8c694ee7a7f9747b2dff8292a86b12c57d74e0
William Yang <williamyang93@gmail.com>
2018-08-30 20:38:32 -0700

    Updated Users data array

--- src/data/users.js
+++ src/data/users.js
@@ -1,10 +1,16 @@
 export const users = [
     {
         email: 'wyang@gmail.com',
+        displayName: 'Will',
+        firstName: 'William',
+        lastName: 'Yang',
         password: '123'
     },
     {
         email: 'jtkd@gmail.com',
+        displayName: 'James',
+        firstName: 'James',
+        lastName: 'Park',
         password: '123'
     }
 ]
