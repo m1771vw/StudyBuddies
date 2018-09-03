ad7cceb78bb4495c4f4a7c4cfc0fcfa557078f3b
James Park <jamestkd918@gmail.com>
2018-09-03 00:35:40 -0700

    #profile page styling

--- src/components/ProfileView.js
+++ src/components/ProfileView.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
 class ProfileView extends Component {
+
     onLogOutClicked = () => {
         this.props.logOut()
     }
     render() {
         return (
-            <div className="level">
-                <div id='dashboard-text'>Welcome To Your Profile, {this.props.currentUser.fullName}!</div>
-                <h1>Email: {this.props.currentUser.email}</h1>
-                <div><button className="button is-danger"onClick={this.onLogOutClicked}>Log Out</button></div>
+            <div>
+                <div className="level profile-level">
+                <div className="box profile-box">
+                    <div id='dashboard-text'>Welcome To Your Profile {this.props.currentUser.displayName}!</div>
+                    <h1>Email: {this.props.currentUser.email}</h1>
+                    <h1>Name: {this.props.currentUser.fullName}</h1>
+                    <h1>Display Name: {this.props.currentUser.displayName}</h1>
+                    
+                </div>
+                </div>
+                <div className="level-item"><button className="button is-danger" onClick={this.onLogOutClicked}>Log Out</button></div>
             </div>
         );
     }
