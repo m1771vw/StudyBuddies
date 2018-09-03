259baed7514726b339bc3be8caaee92f58f0bc25
William Yang <williamyang93@gmail.com>
2018-09-02 20:57:40 -0700

    Modified RANDOM_COLORS

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,11 +8,13 @@ export const VIEW_SET = 'VIEW';
 export const QUIZ = 'QUIZ';
 export const EDIT = "EDIT";
 export const QUIZ_RESULTS = 'QUIZ_RESULTS';
-export const CORRECT_ANSWER = '2px solid green';
-export const WRONG_ANSWER = '2px solid red';
-export const RANDOM_COLORS = ['#698B22', '#33658A', '#2F4858', '#922D50', '#8DAA91', 
-                              '#28112B', '#DD7500', '#FFC469', '#FCD116', '#385E0F',
-                              '#8B3626', '#FF5333', '#CD0000', '#00688B', '#35586C',
-                              '#003F87', '#6A5ACD', '#473C8B', '#00688B', '#68228B']
-
-// export const 
+// export const CORRECT_ANSWER = '2px solid green';
+// export const WRONG_ANSWER = '2px solid red';
+export const RANDOM_COLORS = [  '#922D50', '#8B3626', '#FF5333', '#CD0000', // Red
+                                '#DD7500', '#FFC469',                       // Orange
+                                '#FCD116',                                  // Yellow
+                                '#698B22', '#8DAA91', '#385E0F',            // Green
+                                '#2F4858', '#33658A', '#00688B', '#35586C','#003F87', '#00688B', // Blue
+                                '#28112B', '#6A5ACD', '#68228B'             // Purple
+                             ]
+ 
