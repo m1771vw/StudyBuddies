7e797804b7a69f220b78bd0463029b7dcc0ae237
William Yang <williamyang93@gmail.com>
2019-01-11 20:27:25 -0800

    Fixed responsiveness for mobile part of quiz, need to fix desktop now

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,7 +5,7 @@ import { scrambleCards, selectQuizCards } from '../helpers';
 class Quiz extends Component {
     state = {
         userHasChosen: false,
-        shuffledCardSet: scrambleCards(this.props.selectedCardSet.cards), 
+        shuffledCardSet: scrambleCards(this.props.selectedCardSet.cards),
         quizCardSet: selectQuizCards(this.props.selectedCardSet.cards, 0),
         cardSetIndex: 0,
         endOfSetReached: false,
@@ -30,22 +30,22 @@ class Quiz extends Component {
 
     showNextQuestion = correct => {
         const checkReachedEndOfCardSet = this.state.cardSetIndex === this.state.shuffledCardSet.length - 1
-        if(checkReachedEndOfCardSet) {
-            if(correct){
+        if (checkReachedEndOfCardSet) {
+            if (correct) {
                 this.setState({
                     cardSetIndex: 0,
                     userHasChosen: true,
                     scoreCorrect: this.state.scoreCorrect + 1
-                }, () => {this.props.setupQuizResults(this.state.scoreCorrect, this.state.scoreMissed)});
+                }, () => { this.props.setupQuizResults(this.state.scoreCorrect, this.state.scoreMissed) });
             } else {
                 this.setState({
                     cardSetIndex: 0,
                     userHasChosen: true,
                     scoreMissed: this.state.scoreMissed + 1
-                }, () => {this.props.setupQuizResults(this.state.scoreCorrect, this.state.scoreMissed)});
-            } 
+                }, () => { this.props.setupQuizResults(this.state.scoreCorrect, this.state.scoreMissed) });
+            }
         } else if (correct) {
-            this.getNewSetOfCards(this.state.shuffledCardSet, this.state.cardSetIndex+1);
+            this.getNewSetOfCards(this.state.shuffledCardSet, this.state.cardSetIndex + 1);
             this.setState({
                 userHasChosen: false,
                 cardSetIndex: this.state.cardSetIndex + 1,
@@ -53,7 +53,7 @@ class Quiz extends Component {
                 scoreCorrect: this.state.scoreCorrect + 1
             });
         } else {
-            this.getNewSetOfCards(this.state.shuffledCardSet, this.state.cardSetIndex+1);
+            this.getNewSetOfCards(this.state.shuffledCardSet, this.state.cardSetIndex + 1);
             this.setState({
                 userHasChosen: false,
                 cardSetIndex: this.state.cardSetIndex + 1,
@@ -65,19 +65,21 @@ class Quiz extends Component {
 
     quizTopHalf = () => {
         let cardStatus = <h1>{this.state.cardSetIndex + 1} / {this.state.shuffledCardSet.length}</h1>
-        return(
+        return (
             <div className="level">
-                <div className="quiz-question-flex-container level-item">
-                    <div className='level-item level-left return-to-view-button'>
-                            <button onClick={() => {this.props.selectCardSet(this.props.selectedCardSetIndex)}} className='button is-watermelon'>Return to View</button>
+                <div className="quiz-question-flex-container ">
+                    <div className="quiz-status">
+                        <div className='level-item return-to-view-button'>
+                            <button onClick={() => { this.props.selectCardSet(this.props.selectedCardSetIndex) }} className='button is-watermelon'>Return to View</button>
                         </div>
+                        <div className=" flashcard-counter">
+                            {cardStatus}
+                            <h1 className="green-text">Correct: {this.state.scoreCorrect}</h1>
+                            <h1 className="red-text">Missed: {this.state.scoreMissed}</h1>
+                        </div>
+                    </div>
                     <div className=" box flashcard-box quiz-term-box term-text">
                         <h1> {this.state.shuffledCardSet[this.state.cardSetIndex].term}</h1>
-                    </div>
-                    <div className=" flashcard-counter">
-                        {cardStatus}
-                        <h1 className="green-text">Correct: {this.state.scoreCorrect}</h1>
-                        <h1 className="red-text">Missed: {this.state.scoreMissed}</h1>
                     </div>
                 </div>
             </div>
@@ -85,14 +87,14 @@ class Quiz extends Component {
     }
 
     quizBottomHalf = () => {
-        return(
+        return (
             <div className='level flashcard-answer-container'>
-                {   
+                {
                     this.state.userHasChosen && this.state.quizCardSet[this.state.selectedCardIndex].answer
-                    ? this.answerIsCorrect()
-                    : this.state.userHasChosen && !this.state.quizCardSet[this.state.selectedCardIndex].answer
-                    ? this.answerIsMissed()
-                    : this.noAnswerSelected()
+                        ? this.answerIsCorrect()
+                        : this.state.userHasChosen && !this.state.quizCardSet[this.state.selectedCardIndex].answer
+                            ? this.answerIsMissed()
+                            : this.noAnswerSelected()
                 }
             </div>
         )
@@ -101,86 +103,88 @@ class Quiz extends Component {
     answerIsMissed = () => {
         let correctCard = "box flashcard-box has-text-centered quiz-flashcard-wordwrap is-correct-card";
         let wrongCard = "box flashcard-box has-text-centered quiz-flashcard-wordwrap is-wrong-card";
-        return(
+        return (
             this.state.quizCardSet.map((card, index) => {
-                if(card.answer) {
-                    return(
-                        <div key={card.term+index} className='level-item level-right'>
-                    <div onClick={ () => {this.showNextQuestion(false)}} className={correctCard}>
-                        <h1>Definition: </h1>
-                        <h1>{card.definition}</h1>
-                    </div>
-                </div>
-                    )
-                } else if(this.state.quizCardSet.indexOf(card) === this.state.selectedCardIndex) {
-                    return(
-                        <div key={card.term+index} className='level-item level-right'>
-                    <div onClick={ () => {this.showNextQuestion(false)}} className={wrongCard}>
-                        <h1>Definition: </h1>
-                        <h1>{card.definition}</h1>
-                    </div>
-                </div>
-                    )
-                } else
-                return(
-                    <div key={card.term+index} className='level-item level-right'>
-                    <div onClick={ () => {this.showNextQuestion(false)}} className='box flashcard-box has-text-centered quiz-flashcard-wordwrap'>
-                        <h1>Definition: </h1>
-                        <h1>{card.definition}</h1>
-                    </div>
-                </div>
-                )
-            }   
-        ))
-    }
-
-    answerIsCorrect = () => {
-        let correctCard = "box flashcard-box has-text-centered quiz-flashcard-wordwrap is-correct-card";
-        return(
-            this.state.quizCardSet.map((card, index) => {
-                if(card.answer){
-                    return(
-                        <div key={card.term+index} className='level-item level-right'>
-                        <div onClick={ () => {this.showNextQuestion(true)}} className={correctCard}>
-                        <h1>Definition: </h1>
-                        <h1>{card.definition}</h1>
-                    </div>
-                </div>
-                    )
-                } else {
-                    return(
-                        <div key={card.term+index} className='level-item level-right'>
-                            <div onClick={() => {this.showNextQuestion(false)}} className='box flashcard-box has-text-centered quiz-flashcard-wordwrap'>
+                if (card.answer) {
+                    return (
+                        <div key={card.term + index} className='level-item'>
+                            <div onClick={() => { this.showNextQuestion(false) }} className={correctCard}>
                                 <h1>Definition: </h1>
                                 <h1>{card.definition}</h1>
                             </div>
                         </div>
-                 )
+                    )
+                } else if (this.state.quizCardSet.indexOf(card) === this.state.selectedCardIndex) {
+                    return (
+                        <div key={card.term + index} className='level-item'>
+                            <div onClick={() => { this.showNextQuestion(false) }} className={wrongCard}>
+                                <h1>Definition: </h1>
+                                <h1>{card.definition}</h1>
+                            </div>
+                        </div>
+                    )
+                } else
+                    return (
+                        <div key={card.term + index} className='level-item '>
+                            <div onClick={() => { this.showNextQuestion(false) }} className='box flashcard-box has-text-centered quiz-flashcard-wordwrap'>
+                                <h1>Definition: </h1>
+                                <h1>{card.definition}</h1>
+                            </div>
+                        </div>
+                    )
+            }
+            ))
+    }
+
+    answerIsCorrect = () => {
+        let correctCard = "box flashcard-box has-text-centered quiz-flashcard-wordwrap is-correct-card";
+        return (
+            this.state.quizCardSet.map((card, index) => {
+                if (card.answer) {
+                    return (
+                        <div key={card.term + index} className='level-item'>
+                            <div onClick={() => { this.showNextQuestion(true) }} className={correctCard}>
+                                <h1>Definition: </h1>
+                                <h1>{card.definition}</h1>
+                            </div>
+                        </div>
+                    )
+                } else {
+                    return (
+                        <div key={card.term + index} className='level-item'>
+                            <div onClick={() => { this.showNextQuestion(false) }} className='box flashcard-box has-text-centered quiz-flashcard-wordwrap'>
+                                <h1>Definition: </h1>
+                                <h1>{card.definition}</h1>
+                            </div>
+                        </div>
+                    )
                 }
             })
         )
     }
 
     noAnswerSelected = () => {
-        return(
-            this.state.quizCardSet.map((card,index) => {
-                return(
-                    <div key={card.term+index} className='level-item level-right'>
-                    <div onClick={() => {this.answerClicked(index)}} className="box flashcard-box has-text-centered quiz-flashcard-wordwrap">
-                        <h1>Definition: </h1>
-                        <h1>{card.definition}</h1>
+        return (
+            this.state.quizCardSet.map((card, index) => {
+                return (
+                    <div key={card.term + index} className='level-item'>
+                        <div onClick={() => { this.answerClicked(index) }} className="box flashcard-box has-text-centered quiz-flashcard-wordwrap">
+                            <h1>Definition: </h1>
+                            <h1>{card.definition}</h1>
+                        </div>
                     </div>
-                </div>
-        )}))
+                )
+            }))
     }
 
     render() {
-    return (
-        <div>
-            {this.quizTopHalf()}
-            {this.quizBottomHalf()}
-        </div>
-    )};
+        return (
+            <div>
+                {this.quizTopHalf()}
+                {this.quizBottomHalf()}
+            </div>
+        )
+    };
 }
 
 Quiz.propTypes = {
